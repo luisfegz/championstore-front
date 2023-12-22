5b2e37c76df62aaebf30f1ef3272609fa3acf011
luisfegz <87459956+luisfegz@users.noreply.github.com>
2023-12-21 20:41:56 -0500

    Update CartContext.js

--- components/CartContext.js
+++ components/CartContext.js
@@ -1,38 +1,41 @@
-import {createContext, useEffect, useState} from "react";
+import { createContext, useEffect, useState } from "react";
 
 export const CartContext = createContext({});
 
-export function CartContextProvider({children}) {
-  const ls = typeof window !== "undefined" ? window.localStorage : null;
-  const [cartProducts,setCartProducts] = useState([]);
-  useEffect(() => {
-    if (cartProducts?.length > 0) {
-      ls?.setItem('cart', JSON.stringify(cartProducts));
+export function CartContextProvider({ children }) {
+    const ls = typeof window !== "undefined" ? window.localStorage : null;
+    const [cartProducts, setCartProducts] = useState([]);
+    useEffect(() => {
+        if (cartProducts?.length > 0) {
+            ls?.setItem('cart', JSON.stringify(cartProducts));
+        } else {
+            ls.clear();
+        }
+    }, [cartProducts]);
+    useEffect(() => {
+        if (ls && ls.getItem('cart')) {
+            setCartProducts(JSON.parse(ls.getItem('cart')))
+        }
+    }, [])
+    function addProduct(productId) {
+        setCartProducts(prev => [...prev, productId])
     }
-  }, [cartProducts]);
-  useEffect(() => {
-    if (ls && ls.getItem('cart')) {
-      setCartProducts(JSON.parse(ls.getItem('cart')));
+    function removeProduct(productId) {
+        setCartProducts(prev => {
+            const pos = prev.indexOf(productId);
+            if (pos !== -1) {
+                return prev.filter((value, index) => index !== pos)
+            }
+            return prev;
+        });
     }
-  }, []);
-  function addProduct(productId) {
-    setCartProducts(prev => [...prev,productId]);
-  }
-  function removeProduct(productId) {
-    setCartProducts(prev => {
-      const pos = prev.indexOf(productId);
-      if (pos !== -1) {
-        return prev.filter((value,index) => index !== pos);
-      }
-      return prev;
-    });
-  }
-  function clearCart() {
-    setCartProducts([]);
-  }
-  return (
-    <CartContext.Provider value={{cartProducts,setCartProducts,addProduct,removeProduct,clearCart}}>
-      {children}
-    </CartContext.Provider>
-  );
+    function clearCart() {
+        setCartProducts([]);
+        ls.clear();
+    }
+    return (
+        <CartContext.Provider value={{ cartProducts, setCartProducts, addProduct, removeProduct, clearCart }}>
+            {children}
+        </CartContext.Provider>
+    );
 }
